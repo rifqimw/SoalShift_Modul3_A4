2f48eb58234c39e7f17cfa876829f23bc2ba4136
Ahmad Imam Fadhila <ahmadimamfadhila@gmail.com>
2017-10-25 13:28:44 +0700

    input pindah dari thread ke main, perubahan di thread faktorial

--- soal4.c
+++ soal4.c
@@ -3,69 +3,58 @@
 #include<stdlib.h>
 #include<unistd.h>
 
-pthread_t tid1;
-pthread_t tid2;
-pthread_t tid3;
-int status;
 int i;
-int arr[6];
-
-void* tulis(void *arg)
-{
-	status = 0;
-	int temp[];
-	do
-	{
-	  scanf("%d%c", &arr[i], &temp);
-	  i++;
-	} while(temp!='\n');
-	status = 1;
-	return  NULL;
-}
+pthread_t tid[1000];
 
 void* sort(void *arg)
 {
 	while(status != 1)
 	{
 	}
-	int temp[];
+	int temp[6];
 	for(int j = 0; j < i-1; j++)
 	{
 	  for(int k = 0; k < i-j-1; k++)
 	  {
 	    if(arr[k] > arr[k+1])
 	    {
-	 	temp[] = arr[k+1];
+	 	temp[1] = arr[k+1];
 	 	arr[k+1] = arr[k];
-	 	arr[k] = temp[];
+	 	arr[k] = temp[1];
 	    }
 	  }
 	}
 	status = 2;
-}
-void* faktorial(void *arg)
+}*/
+void* faktorial(void *argv)
 {
-	while(status != 2)
+	//while(status != 2)
+	//{
+	//}
+	int *x;
+	x = (int*)argv;
+	long long faktorial = 1;
+	for (int j = 1; j <= x; j++)
 	{
+		faktorial *= j;
 	}
-	unsigned long long faktorial = 1;
-	for (int j = 0; j < i; j++)
-	{
-		for(int k = 1; k<=arr[j]; ++k)
-		{
-			faktorial *= k;
-		}
-	printf("Hasil %d! = %ull", arr[j], faktorial);
+	printf("Hasil %d! = %lld", x, faktorial);
 	printf("\n");
-	}
 }
-int main(void)
+int main()
 {
-	pthread_create(&(tid1), NULL, &tulis, NULL);
-	pthread_join(tid1, NULL);
-	pthread_create(&(tid2), NULL, &sort, NULL);
-	pthread_join(tid2,NULL);
-	pthread_create(&(tid3), NULL, &faktorial, NULL);
-	pthread_join(tid3, NULL);
+	int i=0;
+	int input[1000];
+	while(scanf("%d",&input[i])!=EOF)
+	{
+		pthread_create(&(tid[i]), NULL, &faktorial,(void*) input[i]); 
+		i++;
+	}
+	//pthread_create(&(tid1), NULL, &tulis, NULL);
+	//pthread_join(tid1, NULL);
+	//pthread_create(&(tid2), NULL, &sort, NULL);
+	//pthread_join(tid2,NULL);
+	//pthread_create(&(tid3), NULL, &faktorial, NULL);
+	//pthread_join(tid3, NULL);
 	return 0;
 }
