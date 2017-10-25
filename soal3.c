c01c104bf55c7b6dee61d74085b014d88e0abd9c
rifqimw <rifqimukti076@gmail.com>
2017-10-25 04:13:40 -0400

    Yang nguranginbaru berjalan sekali kayaknya

--- soal3.c
+++ soal3.c
@@ -3,40 +3,53 @@
 #include <pthread.h>
 #include <unistd.h>
 
-int stat_k=100, stat_l=100, opsi;
+int stat_k=100, stat_l=100, status=0;
 
 void* l_kurang(void *arg)
-{
+{	
 	sleep(10);	
-	stat_l-=15;	
+	stat_l-=15; }
 }
 
 void* k_kurang(void *arg)
-{
+{	
 	sleep(20);
-	stat_k-=10;
+	stat_k-=10; }
+}
+
+void* menu(void *arg)
+{
+	int opsi;
+	while(status==0)
+	{
+		printf("\nGAME AQUARIUM -- Lohan dan Kepiting\n");
+		printf("Input sesuai pilihan yang Anda inginkan:\n");
+		printf("1.Beri makan lohan\n2.Beri makan kepiting\n");
+		printf("Status kepiting : %d dan Status lohan : %d\n", stat_k, stat_l);
+		scanf("%d", &opsi);
+		if(opsi==1) stat_l+=10;
+		else if(opsi==2) stat_k+=10;	
+		else printf("Input error!");
+
+		if(stat_k <= 0 || stat_l <= 0 || stat_k > 100 || stat_l > 100) status=1;
+	}
 }
 
 int main()
 {
 	pthread_t thr1, thr2, thr3, thr4, thr5;
 
+	system("clear");
 	pthread_create(&thr1, NULL, &l_kurang, NULL); 	
 	pthread_create(&thr2, NULL, &k_kurang, NULL); 
+	pthread_create(&thr3, NULL, &menu, NULL);
 
-	while(1)
-	{
-		if(stat_k <= 0 || stat_l <= 0 || stat_k > 100 || stat_l > 100) break;
-		else {
-		printf("\nGAME AQUARIUM -- Lohan dan Kepiting\n");
-		printf("Input sesuai pilihan yang Anda inginkan:\n");
-		printf("1.Beri makan lohan\n2.Beri makan kepiting\n3.Cek status\n");
-		scanf("%d", &opsi);
-		if(opsi==1) stat_l+=10;
-		else if(opsi==2) stat_k+=10;	
-		else if(opsi==3) printf("Status lohan : %d. Status kepiting : %d\n", stat_l, stat_k);
-		else printf("Input error!");
-		}
-	}
+	//if(stat_k <= 0 || stat_l <= 0 || stat_k > 100 || stat_l > 100) status=1;
+
+	pthread_join(thr3, NULL);
+	pthread_join(thr1, NULL);
+	pthread_join(thr2, NULL);
+
+	printf("GAME AQUARIUM SELESAI! Terima kasih sudah bermain :)\n");
 	return 0;
 }
