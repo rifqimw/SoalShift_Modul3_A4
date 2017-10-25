043519e1aef74988f9bfd05e5deded4e6b1b2b59
rifqimw <rifqimukti076@gmail.com>
2017-10-25 03:43:00 -0400

    Thread tambah dimasukin ke main(), mungkin selesai(?)

--- soal3.c
+++ soal3.c
@@ -3,35 +3,18 @@
 #include <pthread.h>
 #include <unistd.h>
 
-int stat_k=100, stat_l=100, status=0, opsi;
-
-void* menu(void *arg)
-{
-}
+int stat_k=100, stat_l=100, opsi;
 
 void* l_kurang(void *arg)
 {
-	stat_l-=15;
-	sleep(10);		
+	sleep(10);	
+	stat_l-=15;	
 }
 
-void *k_kurang(void *arg)
+void* k_kurang(void *arg)
 {
-	stat_k-=10;
 	sleep(20);
-}
-
-void *l_tambah(void *arg)
-{
-	while(status==0) {}	
-	if(opsi==1)
-	{
-		stat_l += 10;
-	}
-	if(opsi==2)
-	{
-		stat_k += 10;
-	}
+	stat_k-=10;
 }
 
 int main()
@@ -39,19 +22,20 @@ int main()
 	pthread_t thr1, thr2, thr3, thr4, thr5;
 
 	pthread_create(&thr1, NULL, &l_kurang, NULL); 	
-	pthread_create(&thr2, NULL, &k_kurang, NULL);
-	pthread_create(&thr3, NULL, &tambah, NULL); 	
-	pthread_create(&thr5, NULL, &menu, NULL); 	 	
+	pthread_create(&thr2, NULL, &k_kurang, NULL); 
 
 	while(1)
 	{
 		if(stat_k <= 0 || stat_l <= 0 || stat_k > 100 || stat_l > 100) break;
 		else {
-			pthread_join(thr1, NULL);
-			pthread_join(thr2, NULL);
-			pthread_join(thr3, NULL);
-			pthread_join(thr4, NULL);
-			pthread_join(thr5, NULL);
+		printf("\nGAME AQUARIUM -- Lohan dan Kepiting\n");
+		printf("Input sesuai pilihan yang Anda inginkan:\n");
+		printf("1.Beri makan lohan\n2.Beri makan kepiting\n3.Cek status\n");
+		scanf("%d", &opsi);
+		if(opsi==1) stat_l+=10;
+		else if(opsi==2) stat_k+=10;	
+		else if(opsi==3) printf("Status lohan : %d. Status kepiting : %d\n", stat_l, stat_k);
+		else printf("Input error!");
 		}
 	}
 	return 0;
